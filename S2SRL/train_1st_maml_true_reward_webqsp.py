0ac19bd63a39d233de5e3e04f5732ff1f0b34205
goka <535772489@qq.com>
2020-01-12 14:52:11 +0800

    update webqsp maml

--- S2SRL/train_1st_maml_true_reward_webqsp.py
+++ S2SRL/train_1st_maml_true_reward_webqsp.py
@@ -7,14 +7,14 @@ import logging
 import numpy as np
 from tensorboardX import SummaryWriter
 
-from libbots import data, model, utils, metalearner
+from libbots import data, model, utils, metalearner_webqsp
 
 import torch
 import torch.optim as optim
 import time
 import ptan
 
-SAVES_DIR = "../data/saves"
+SAVES_DIR = "../data/saves/webqsp"
 
 MAX_EPOCHES = 30
 MAX_TOKENS = 40
@@ -24,8 +24,8 @@ GAMMA = 0.05
 DIC_PATH = '../data/webqsp_data/share.question'
 TRAIN_QUESTION_ANSWER_PATH = '../data/webqsp_data/mask_even_10.0%/RL_train_TR_sub_webqsp.json'
 TRAIN_WEBQSP_QUESTION_ANSWER_PATH = '../data/webqsp_data/RL_train_TR_sub_webqsp.json'
-DICT_WEBQSP = '../data/webqsp_data/CSQA_result_question_type_944K.json'
-DICT_WEBQSP_WEAK = '../data/webqsp_data/CSQA_result_question_type_count944K.json'
+DICT_WEBQSP = '../data/webqsp_data/top5_webqsp_train_all_week.json'
+DICT_WEBQSP_WEAK = '../data/webqsp_data/top5_webqsp_train_all_week.json'
 log = logging.getLogger("train")
 
 # Calculate 0-1 sparse reward for samples in test dataset to judge the performance of the model.
@@ -48,7 +48,7 @@ def run_test(test_data, net, rev_emb_dict, end_token, device="cuda"):
             if temp_idx in rev_emb_dict and rev_emb_dict.get(temp_idx) != '#END':
                 action_tokens.append(str(rev_emb_dict.get(temp_idx)).upper())
         # Using 0-1 reward to compute accuracy.
-        argmax_reward_sum += float(utils.calc_True_Reward(action_tokens, p2, False))
+        argmax_reward_sum += float(utils.calc_True_Reward_webqsp(action_tokens, p2, False))
         # argmax_reward_sum += random.random()
         argmax_reward_count += 1
     return float(argmax_reward_sum) / float(argmax_reward_count)
@@ -58,8 +58,8 @@ if __name__ == "__main__":
     # # command line parameters
     # # -a=True means using adaptive reward to train the model. -a=False is using 0-1 reward.
     # sys.argv = ['train_maml_true_reward_webqsp.py', '--cuda', '-l=../data/saves/rl_even_TR_batch8_1%/truereward_0.739_29.dat', '-n=maml_1%_batch8_att=0_test', '-s=5', '-a=0', '--att=0', '--lstm=1', '--fast-lr=0.1', '--meta-lr=1e-4', '--steps=5', '--batches=1', '--weak=1']
-    sys.argv = ['train_1st_maml_true_reward_webqsp.py', '-l=../data/saves/webqsp/rl_even_adaptive_1%_att=1/truereward_0.614_00.dat',
-                '-n=maml_att=0_newdata2k_1storder1', '--cuda', '-s=5', '-a=0', '--att=0', '--lstm=1', '--fast-lr=0.1',
+    sys.argv = ['train_1st_maml_true_reward_webqsp.py', '-l=../data/saves/webqsp/rl_even_adaptive_1%_att=1/pre_bleu_0.928_06.dat',
+                '-n=maml_att=0_newdata2k_1storder1', '--cuda', '-s=5', '-a=0', '--att=1', '--lstm=1', '--fast-lr=0.1',
                 '--meta-lr=1e-4', '--steps=5', '--batches=1', '--weak=1', '--embed-grad']
     parser = argparse.ArgumentParser()
     parser.add_argument("--cuda", action='store_true', default=False, help="Enable cuda")
@@ -83,7 +83,7 @@ if __name__ == "__main__":
     # Conversely, if action is 'store_false', if the parameter has a value, the parameter is viewed as 'False'.
     parser.add_argument('--first-order', action='store_true', help='use the first-order approximation of MAML')
     # If false, the embedding tensors in the model do not need to be trained.
-    parser.add_argument('--embed-grad', action='store_false', help='use the first-order approximation of MAML')
+    parser.add_argument('--embed-grad', action='store_false', help='fix embeddings when training')
     parser.add_argument('--fast-lr', type=float, default=0.0001,
                         help='learning rate for the 1-step gradient update of MAML')
     parser.add_argument('--meta-lr', type=float, default=0.0001,
@@ -117,10 +117,10 @@ if __name__ == "__main__":
     train_data_webqsp = data.encode_phrase_pairs_RLTR(phrase_pairs_webqsp, emb_dict)
     train_data_webqsp = data.group_train_data_RLTR_for_support(train_data_webqsp)
 
-    # dictwebqsp = data.get_webqsp(DICT_WEBQSP)
-    # log.info("Reading dict_webqsp from %s is done. %d pairs in dict_webqsp.", DICT_WEBQSP, len(dictwebqsp))
-    # dictwebqsp_weak = data.get_webqsp(DICT_WEBQSP_WEAK)
-    # log.info("Reading dict_webqsp_weak from %s is done. %d pairs in dict_webqsp_weak", DICT_WEBQSP_WEAK, len(dictwebqsp_weak))
+    dictwebqsp = data.get_webqsp(DICT_WEBQSP)
+    log.info("Reading dict_webqsp from %s is done. %d pairs in dict_webqsp.", DICT_WEBQSP, len(dictwebqsp))
+    dictwebqsp_weak = data.get_webqsp(DICT_WEBQSP_WEAK)
+    log.info("Reading dict_webqsp_weak from %s is done. %d pairs in dict_webqsp_weak", DICT_WEBQSP_WEAK, len(dictwebqsp_weak))
 
     rand = np.random.RandomState(data.SHUFFLE_SEED)
     rand.shuffle(train_data)
@@ -165,7 +165,7 @@ if __name__ == "__main__":
     beg_token = torch.LongTensor([emb_dict[data.BEGIN_TOKEN]]).to(device)
     beg_token = beg_token.cuda()
 
-    metaLearner = metalearner.MetaLearner(net, device=device, beg_token=beg_token, end_token=end_token, adaptive=args.adaptive, samples=args.samples, train_data_support_944K=train_data_944K, rev_emb_dict=rev_emb_dict, first_order=args.first_order, fast_lr=args.fast_lr, meta_optimizer_lr=args.meta_lr, dial_shown=False, dict=dict944k, dict_weak=dict944k_weak, steps=args.steps, weak_flag=args.weak)
+    metaLearner = metalearner_webqsp.MetaLearner(net, device=device, beg_token=beg_token, end_token=end_token, adaptive=args.adaptive, samples=args.samples, train_data_support_944K=train_data_webqsp, rev_emb_dict=rev_emb_dict, first_order=args.first_order, fast_lr=args.fast_lr, meta_optimizer_lr=args.meta_lr, dial_shown=False, dict=dictwebqsp, dict_weak=dictwebqsp_weak, steps=args.steps, weak_flag=args.weak)
     log.info("Meta-learner: %d inner steps, %f inner learning rate, "
              "%d outer steps, %f outer learning rate, using weak mode:%s"
              %(args.steps, args.fast_lr, args.batches, args.meta_lr, str(args.weak)))
